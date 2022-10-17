0d57abb2687b8e4009514831c7df51a4e130a5e1
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-10-17 15:41:04 -0300

    ...

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -5,38 +5,53 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            string produto1 = "Computador";
-            string produto2 = "Mesa de escritório";
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
 
-            byte idade = 30;
-            int codigo = 5290;
-            char genero = 'M';
-
-            double preco1 = 2100.0;
-            double preco2 = 650.50;
-            double medida = 53.234567;
-
-            Console.WriteLine("Produtos: ");
-            Console.WriteLine("{0}, cujo preço é ${1:F2}", produto1, preco1);
-            Console.WriteLine($"{produto2}, cujo preço é ${preco2:F2}");
-
-            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
-
-            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
-            Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
-            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3",CultureInfo.InvariantCulture));
+            /*
+            *(3)SPLIT 
+            string s = Console.ReadLine(); //Outra maneira é tirar essa parte..
+            string[] vet = s.Split(' ');   //string[] vet = Console.ReadLine().Split(' ');
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+            Console.WriteLine(p2);
+            */
 
 
             /*
+            *(2)SAÍDA DE DADOS
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+            Console.WriteLine("Produtos: ");
+            Console.WriteLine("{0}, cujo preço é ${1:F2}", produto1, preco1);
+            Console.WriteLine($"{produto2}, cujo preço é ${preco2:F2}");
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3",CultureInfo.InvariantCulture));
+            */
+
+
+            /*
+            *(1)SAÍDA DE DADOS
             int idade = 32;
             double saldo = 10.35784;
             string nome = "Maria";
-
             Console.WriteLine("{0} com {1} anos de idade tem R${2:F2} de saldo no banco.", nome, idade, saldo);
-
             Console.WriteLine($"{nome} tem {idade} anos e R${saldo:F2} no banco.");
-
             // Essa é a única forma possivel de passar o (.) no lugar da (,) usando => "saldo".ToString("F2",CultureInfo.InvariantCulture)
             Console.WriteLine(nome + " tem " + idade + " anos de idade e " + saldo.ToString("F2",CultureInfo.InvariantCulture) + " de reais no banco." ); 
             */
