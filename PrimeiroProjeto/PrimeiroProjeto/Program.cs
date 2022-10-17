3da47f18cfebf4ee72b1857cfd747333cfbf68de
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-10-17 13:51:39 -0300

    ...

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -17,14 +17,15 @@ namespace Course {
             double preco2 = 650.50;
             double medida = 53.234567;
 
-            Console.WriteLine("Produto: ");
-            Console.WriteLine("Computador, cujo preço é ${0:F2}", preco1);
+            Console.WriteLine("Produtos: ");
+            Console.WriteLine("{0}, cujo preço é ${1:F2}", produto1, preco1);
+            Console.WriteLine($"{produto2}, cujo preço é ${preco2:F2}");
 
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
 
-
-
-
-
+            Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F3",CultureInfo.InvariantCulture));
 
 
             /*
