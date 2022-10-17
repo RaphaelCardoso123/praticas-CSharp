66ed909711b16fc6e08c1399dd5ef9f81dab2a54
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-10-17 13:12:59 -0300

    Add project files.

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+
+
+
+
+
+
+
+
+
+            /*
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.WriteLine("{0} com {1} anos de idade tem R${2:F2} de saldo no banco.", nome, idade, saldo);
+
+            Console.WriteLine($"{nome} tem {idade} anos e R${saldo:F2} no banco.");
+
+            // Essa é a única forma possivel de passar o (.) no lugar da (,) usando => "saldo".ToString("F2",CultureInfo.InvariantCulture)
+            Console.WriteLine(nome + " tem " + idade + " anos de idade e " + saldo.ToString("F2",CultureInfo.InvariantCulture) + " de reais no banco." ); 
+            */
+              
+        }
+    }
+}
