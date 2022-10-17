9bc9a5b5c3d647f08896a7433ca8bf323ae48a73
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-10-17 13:31:40 -0300

    ...

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -6,6 +6,19 @@ namespace Course {
         static void Main(string[] args) {
 
 
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produto: ");
+            Console.WriteLine("Computador, cujo preço é ${0:F2}", preco1);
 
 
 
