c9f6399dd80c988e32be4ced052f311989e536b2
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2022-10-17 22:32:37 -0300

    ...

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -50,16 +50,15 @@ namespace Course {
             */
 
 
-            /*
-            *(1) SAÍDA DE DADOS / CONCATENAÇÃO, CULTUREINFO.INVARIANTCULTURE
-            int idade = 32;
+          
+            int idade = 59;
             double saldo = 10.35784;
             string nome = "Maria";
             Console.WriteLine("{0} com {1} anos de idade tem R${2:F2} de saldo no banco.", nome, idade, saldo);
             Console.WriteLine($"{nome} tem {idade} anos e R${saldo:F2} no banco.");
             // Essa é a única forma possivel de passar o (.) no lugar da (,) usando => "saldo".ToString("F2",CultureInfo.InvariantCulture)
             Console.WriteLine(nome + " tem " + idade + " anos de idade e " + saldo.ToString("F2",CultureInfo.InvariantCulture) + " de reais no banco." ); 
-            */
+         
               
         }
     }
