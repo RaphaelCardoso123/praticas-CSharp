3395ee22b41ac10dd983f3945005436148abe1e7
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-11-08 11:42:31 -0300

    ...

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -13,14 +13,14 @@ namespace Course {
 
             double soma = 0;
             for (int i = 1; i <= quantNotas; i++) {
-                Console.Write("Digite a " + i + "° nota: ");
+                Console.WriteLine("Digite a " + i + "° nota: ");
                 double notas = Convert.ToDouble(Console.ReadLine());
                 soma += notas;
             }
 
             double media;
             media = soma / quantNotas;
-            Console.WriteLine("A média do aluno foi de " + media);
+            Console.WriteLine("A média do aluno foi de: " + media);
 
             if (media >= 6) {
                 Console.WriteLine("Parabéns, você passou!!");
@@ -28,17 +28,17 @@ namespace Course {
                 Console.WriteLine("Você reprovou!!");
             }
 
-            Console.Write("Deseja calcular outra média (1/2)? ");
+            Console.WriteLine("Deseja calcular outra média?");
+            Console.WriteLine("Digite 1 para sim");
             int outraMedia = Convert.ToInt32(Console.ReadLine());
 
             switch (outraMedia) {
                 case 1:
                     CalcularMedia();
-                    break;
-                default:
-                    break;
+                break;
+             default:
+                break;
             }
-
         }
         static void Main(string[] args) {
 
