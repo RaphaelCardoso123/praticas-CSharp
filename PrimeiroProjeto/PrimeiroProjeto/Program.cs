4d45b9ec92b7c5b9b3386e42d7258c87558632e3
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-11-08 13:29:38 -0300

    ...

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -479,40 +479,7 @@ namespace Course {
             */
 
 
-            /*
-    while (true) {
-                Console.Write("Digite a quantidade de notas que serão calculadas: ");
-                int qNotas = Convert.ToInt32(Console.ReadLine());
-                double sumNotas = 0;
-        char continuar = '.';
-                for (int nota = 1; nota <= qNotas; nota++) {
-                Console.Write($"Digite a {nota}° nota: ");
-                sumNotas += Convert.ToDouble(Console.ReadLine());
-                }
-                double media = Math.Round(sumNotas / qNotas, 2);
-                Console.WriteLine($"A média do aluno é de: {media}");
-        if (media >= 6) {
-            Console.WriteLine("Está Aprovado");
-        }
-        else {
-            Console.WriteLine("Está Repovado");
-        }
-        do {
-            try {
-                Console.Write("Deseja escrever a nota para outro aluno(S/N) ?: ");
-                continuar = Convert.ToChar(Console.ReadLine().ToLower());
-            }
-            catch {
-                Console.WriteLine("Digite um opção valida (S/N)");
-            }
-        } while (continuar != 's' && continuar != 'n');
-
-        if (continuar == 'n') {
-            Console.WriteLine("Até logo!");
-            break;
-        }
-    }
-    */
+           
 
             /*
             *(8) EXERCICIOS 
