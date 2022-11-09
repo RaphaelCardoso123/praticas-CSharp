c827a81021e280042a64d9dbdda75e93e3cc1f7e
RaphaelCardoso123 <91799151+RaphaelCardoso123@users.noreply.github.com>
2022-11-09 10:43:01 -0300

    ...

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -11,40 +11,34 @@ namespace Course {
 
             //---------------------------------------------------------------------------------
             static void CalcularMedia() {
-
-            Console.WriteLine("Digite a quantidade de notas a ser calculada: ");
-            int quantNotas = Convert.ToInt32(Console.ReadLine());
-
-            double soma = 0;
-            for (int i = 1; i <= quantNotas; i++) {
-                Console.WriteLine("Digite a " + i + "° nota: ");
-                double notas = Convert.ToDouble(Console.ReadLine());
-                soma += notas;
+                Console.WriteLine("Digite a quantidade de notas a ser calculada: ");
+                int quantNotas = Convert.ToInt32(Console.ReadLine());
+                double soma = 0;
+                for (int i = 1; i <= quantNotas; i++) {
+                    Console.WriteLine("Digite a " + i + "° nota: ");
+                    double notas = Convert.ToDouble(Console.ReadLine());
+                    soma += notas;
+                }
+                double media;
+                media = Math.Round((soma / quantNotas), 1);
+                Console.WriteLine("A média do aluno foi de: " + media);
+                if (media >= 6) {
+                    Console.WriteLine("Parabéns, você passou!!");
+                }
+                else {
+                    Console.WriteLine("Você ficou de recuperação!!");
+                }
+                Console.WriteLine("Deseja calcular outra média?");
+                Console.WriteLine("Digite 1 para sim");
+                int outraMedia = Convert.ToInt32(Console.ReadLine());
+                switch (outraMedia) {
+                    case 1:
+                        CalcularMedia();
+                        break;
+                    default:
+                        break;
+                }
             }
-
-            double media;
-            media = Math.Round((soma / quantNotas), 1);
-            Console.WriteLine("A média do aluno foi de: " + media);
-
-            if (media >= 6) {
-                Console.WriteLine("Parabéns, você passou!!");
-            }
-            else {
-                Console.WriteLine("Você ficou de recuperação!!");
-            }
-
-            Console.WriteLine("Deseja calcular outra média?");
-            Console.WriteLine("Digite 1 para sim");
-            int outraMedia = Convert.ToInt32(Console.ReadLine());
-
-            switch (outraMedia) {
-                case 1:
-                    CalcularMedia();
-                    break;
-                default:
-                    break;
-            }
-        }
             CalcularMedia();
             //---------------------------------------------------------------------------------
 
