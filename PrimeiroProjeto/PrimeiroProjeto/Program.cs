90f1d7683417612a81e79ba44bd47551f1cd2260
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2022-11-19 12:20:22 -0300

    ...

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -10,29 +10,26 @@ namespace Course {
         static void Main(string[] args) {
 
 
-
-
-
-
-            /*
             //(34) EXPRESSÃO CONDICIONAL TENÁRIA
+
             Console.Write("Digite o preço: ");
             double preco = double.Parse(Console.ReadLine());
+
             //USANDO IF
-            //double desconto;
-            //if (preco < 20.0)
-            //{
-            //    desconto = preco * 0.1;
-            //}
-            //else
-            //{
-            //    desconto = preco * 0.05;
-            //}
-            //
+            /*double desconto;
+            if (preco < 20.0)
+            {
+                desconto = preco * 0.1;
+            }
+            else
+            {
+                desconto = preco * 0.05;
+            }*/
+            
             //USANDO CONDICIONAL TENÁRIA
             double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
             Console.WriteLine($"O valor do desconto é de {desconto}");
-            */
+            
 
 
 
