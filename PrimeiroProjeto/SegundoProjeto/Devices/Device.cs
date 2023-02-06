dfce4574452f5a1481864cacfe9d00ba0b3094f8
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2023-02-06 19:31:50 -0300

    segundo projeto estudos

---
+++ PrimeiroProjeto/SegundoProjeto/Devices/Device.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundoProjeto.Devices
+{
+    abstract class Device
+    {
+        public int SerialNumber { get; set; }
+
+        public abstract void ProcessDoc(string document);
+    }
+}
---
+++ PrimeiroProjeto/SegundoProjeto/Devices/Printer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SegundoProjeto.Devices
+{
+    class Printer : Device
+    {
+        public override void ProcessDoc(string document)
+        {
+            Console.WriteLine("Printer processing: " + document);
+        }
+        public void Print(string document)
+        {
+            Console.WriteLine("Printer print" + document);
+        }
+    }
+}
