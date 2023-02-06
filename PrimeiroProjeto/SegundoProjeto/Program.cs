7fdd37cc8bd291e5548cd8677823d45796f66a76
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2023-02-06 19:07:44 -0300

    ...

---
+++ PrimeiroProjeto/SegundoProjeto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SegundoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando segundo projeto de estudo!");
+            Console.ReadLine();
+        }
+    }
+}
